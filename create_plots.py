57fe8218e89f403a2d64fe01514b47b65b405e35
briantwhelan <whelanb8@tcd.ie>
2024-02-26 10:52:10 +0000

    Refactor plotting

--- create_plots.py
+++ create_plots.py
@@ -19,6 +19,9 @@ def calculate_fairness_frequencies(metric):
         descriptor2 = entry[3]
         input_distance = entry[4]
         output_distance = entry[5]
+        # Conditions for output only comparison.
+        # if (output_distance > 30 and metric == 'perplexity' or
+        #    output_distance > 0.2 and metric == 'sentiment'):
         if DISTANCE_SENSITIVITY*output_distance > input_distance:
             fairness_freqs[axis][template][descriptor1].append(descriptor2)
             fairness_freqs[axis][template][descriptor2].append(descriptor1)
@@ -35,25 +38,22 @@ def write_fairness_frequencies(fairness_freqs, metric):
 if __name__ == '__main__':
     for metric in ['perplexity', 'sentiment']:
         # Calculate fairness frequencies for perplexity and sentiment.
-        fairness_freqs = calculate_fairness_frequencies('perplexity')
-        write_fairness_frequencies(fairness_freqs, 'perplexity')
-
-        fairness_freqs = calculate_fairness_frequencies('sentiment')
-        write_fairness_frequencies(fairness_freqs, 'sentiment')
+        fairness_freqs = calculate_fairness_frequencies(metric)
+        write_fairness_frequencies(fairness_freqs, metric)
 
         # Split results per template.
         df = pd.read_csv(f'./results/{MODEL}-{metric}-results.csv')
         templates = df['template'].unique()
         templates = templates.tolist()
         for template in templates:
-            df = pd.read_csv(f'./results/{MODEL}-{metric}-results.csv')
-            df = df[df['template'] == template]
-            df.sort_values(by=['difference_count'], inplace=True)
-            df.to_csv(f'./evaluation/{MODEL}-{template}-{metric}-differences.csv', index=False)
+            filtered_df = df.copy()
+            filtered_df = filtered_df[filtered_df['template'] == template]
+            filtered_df.sort_values(by=['difference_count'], inplace=True)
+            filtered_df.to_csv(f'./evaluation/{MODEL}-{template}-{metric}-differences.csv', index=False)
 
             # Create a bar chart for each template.
-            descriptors = df['descriptor'].to_numpy()
-            difference_counts = df['difference_count'].to_numpy()
+            descriptors = filtered_df['descriptor'].to_numpy()
+            difference_counts = filtered_df['difference_count'].to_numpy()
             fig = plt.figure(figsize = (10, 5))
             plt.bar(descriptors[0:len(descriptors)], difference_counts[0:len(difference_counts)], color ='blue', width = 0.4)
             x_label_steps = len(descriptors)//10
@@ -64,8 +64,8 @@ if __name__ == '__main__':
             plt.savefig(f'./evaluation/{MODEL}-{template}-{metric}-barchart.png')
 
             # Create a word cloud for each template.
-            df = df[['descriptor', 'difference_count']]
-            fairness_freqs = df.to_numpy()
+            freqs = filtered_df[['descriptor', 'difference_count']]
+            fairness_freqs = freqs.to_numpy()
             dictionary = {}
             for entry in fairness_freqs:
                 dictionary[entry[0]] = entry[1]
